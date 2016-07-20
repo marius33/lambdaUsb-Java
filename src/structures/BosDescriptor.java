68ef6128a6e3c02cff50c3fa25ec51a5eabce0fe
Marius Lazar <marius.lazar33@gmail.com>
2016-07-20 21:55:28 +0300

    Started implementing functions for OOP

--- src/structures/BosDescriptor.java
+++ src/structures/BosDescriptor.java
@@ -4,4 +4,11 @@ package structures;
  * Created by Radu on 19/07/2016.
  */
 public class BosDescriptor {
+
+    private org.usb4java.BosDescriptor libusbBosDescriptor;
+
+    public int bLength(){
+        return libusbBosDescriptor.bLength();
+    }
+
 }
